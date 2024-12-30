aa5ff4821959535403b91776055e088fc749f9b9
aledear <veronica.rleda@gmail.com>
2024-12-30 05:46:38 -0500

    building dies sound

--- Assets/Scripts/Obstacles/CrumblesOnVelocity.cs
+++ Assets/Scripts/Obstacles/CrumblesOnVelocity.cs
@@ -6,7 +6,8 @@ public class CrumblesOnVelocity : MonoBehaviour
 {
     [SerializeField] GameObject crumbledObject;
     public float threshold;
-    public int health = 100; 
+    public int health = 100;
+    [SerializeField] private AudioClip destroyedSound;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -25,6 +26,7 @@ public class CrumblesOnVelocity : MonoBehaviour
     private void Crumble()
     {
         crumbledObject.SetActive(true);
+        AudioManager.Instance.PlayOneShotWithRandomPitch(destroyedSound, false, 0.8f, 1.2f, 0.4f); 
         gameObject.SetActive(false);
         Debug.Log("Object crumbled!");
         Destroy(crumbledObject,10f);
